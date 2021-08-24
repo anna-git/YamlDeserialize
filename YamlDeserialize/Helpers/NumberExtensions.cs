7327fe072a8119f51dd19ef118d4f26beb232488
Anna <anna.yafi@datadoghq.com>
2021-08-24 18:56:55 +0200

    lighten 2

--- YamlDeserialize/Helpers/NumberExtensions.cs
+++ YamlDeserialize/Helpers/NumberExtensions.cs
@@ -23,9 +23,6 @@ namespace YamlDotNet.Helpers
 {
     internal static class NumberExtensions
     {
-        public static bool IsPowerOfTwo(this int value)
-        {
-            return (value & (value - 1)) == 0;
-        }
+        public static bool IsPowerOfTwo(this int value) => (value & (value - 1)) == 0;
     }
 }
