0e77a895ea269076e842f2330d2a5a9d65a27975
Anna <anna.yafi@datadoghq.com>
2021-08-24 17:27:43 +0200

    Add project files.

---
+++ YamlDeserialize.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace YamlDeserialize.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+            var sr = Assembly.GetExecutingAssembly().GetManifestResourceStream("YamlDeserialize.Console.rules.yml");
+            var reader = new StreamReader(sr);
+            var res = deserializer.Deserialize(reader);
+        }
+    }
+}
